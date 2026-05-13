5f4f7ec18de20001f4850b3ca8bc9390dbddf89c
MangukiyaKrisha01 <mangukiyakrisha143@gmail.com>
2026-05-13 19:12:58 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
 $host = 'localhost';
-$db   = 'contact_db';   // change this if needed
-$user = 'root';          // XAMPP default
-$pass = '';              // XAMPP default (empty)
+$db   = 'contact_db';   
+$user = 'root';          
+$pass = '';              
 $charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
